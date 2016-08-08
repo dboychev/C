99fcb0214813b02eb3314322d076b485f3210a0a
dboychev <dboychev95@gmail.com>
2016-08-08 17:13:08 +0300

    Push function added
    A new function added to Linked Lists - Push.

---
+++ Linked Lists/Push.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+	int data;
+	struct node* next;
+};
+
+void Push(struct node** head, int newData)
+{
+	struct node* newNode = malloc(sizeof(head));
+	newNode->data = newData;
+	newNode->next = head;
+	*head = newNode;
+}
+
+main()
+{
+	struct node* first = malloc(sizeof(first));
+	struct node* second = malloc(sizeof(second));
+	first->data = 5;
+	first->next = second;
+	second->data = 3;
+	second->next = NULL;
+
+	Push(first, 4);
+}
