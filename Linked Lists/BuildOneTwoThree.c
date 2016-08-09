c79bf04571487653cfcf579b1facf2ee0828fa81
dboychev <dboychev95@gmail.com>
2016-08-09 16:39:42 +0300

    Build123 updated
    Linked List: Build123 function improved.

--- Linked Lists/BuildOneTwoThree.c
+++ Linked Lists/BuildOneTwoThree.c
@@ -8,19 +8,16 @@ struct node
 
 struct node* BuildOneTwoThree()
 {
-	struct node first;
-	struct node second;
-	struct node third;
-	struct node fourth;
-	first.data = 1;
-	first.next = &second;
-	second.data = 5;
-	second.next = &third;
-	third.data = 10;
-	third.next = &fourth;
-	fourth.data = first.data + second.data;
-	fourth.next = NULL;
-	return &first;
+	struct node* first = malloc(sizeof(struct node));
+	struct node* second = malloc(sizeof(struct node));
+	struct node* third = malloc(sizeof(struct node));
+	first->data = 1;
+	first->next = second;
+	second->data = 2;
+	second->next = third;
+	third->data = 3;
+	third->next = NULL;
+	return first;
 }
 
 main()
